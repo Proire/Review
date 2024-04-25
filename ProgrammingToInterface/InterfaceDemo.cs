aaf2b4b5d12091b3a3ed4087ecc4ab8c4ca7f637
Proire <prasadahire4501@gmail.com>
2024-04-25 17:08:28 +0530

    [Prasad] Add : Completed the task for Review suceesfully and tested it.

---
+++ ProgrammingToInterface/InterfaceDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingToInterface
+{
+    internal class InterfaceDemo
+    {
+        // Interface 
+        public interface ImessageService
+        {
+            void SendMessage(string address);
+        }
+
+        // class implementating interface
+        internal class Email : ImessageService
+        {
+            public void SendMessage(string address)
+            {
+                Console.WriteLine("Sending Email to {0}", address);
+            }
+        }
+
+        // class implementating interface
+        internal class Sms : ImessageService
+        {
+            public void SendMessage(string address)
+            {
+                Console.WriteLine("Sending Sms to {0}", address);
+            }
+        }
+
+        // static class to hold for static method to implement programming to interface
+        internal static class Messageprovider
+        {
+            public static void Send(ImessageService s, string add)
+            {
+                s.SendMessage(add);
+
+            }
+        }
+    }
+}
---
+++ ProgrammingToInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using static ProgrammingToInterface.InterfaceDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome the Messaging Application \n");
+
+        // imagine these are objects created by user at the runtime 
+        Sms sms = new();
+        Email email = new();
+
+        // Passed to this static method at runtime so program to interface
+        Messageprovider.Send(sms, "Vita");
+        Messageprovider.Send(email, "Vita");
+
+    }
+}
